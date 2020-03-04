485e34a199f95b8c2850bff6020e08c04134f6ee
achine <a291774405@vip.qq.com>
2020-03-04 18:11:04 +0800

    add Order and OrderState class

---
+++ shopping-entity/src/main/java/edu/nf/shopping/order/entity/Order.java
@@ -0,0 +1,110 @@
+package edu.nf.shopping.order.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author Achine
+ * @date 2020/3/4
+ */
+public class Order {
+    private String orderId;
+    private BigDecimal buyPrice;
+    private BigDecimal cheapPrice;
+    private BigDecimal transportPirce;
+    private Date buyTime;
+    private String buyAdmin;
+    private Integer collectRegion;
+    private String collectAddress;
+    private String buyRemark;
+    private String orderRemark;
+    private OrderState orderState;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public BigDecimal getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(BigDecimal buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public BigDecimal getCheapPrice() {
+        return cheapPrice;
+    }
+
+    public void setCheapPrice(BigDecimal cheapPrice) {
+        this.cheapPrice = cheapPrice;
+    }
+
+    public BigDecimal getTransportPirce() {
+        return transportPirce;
+    }
+
+    public void setTransportPirce(BigDecimal transportPirce) {
+        this.transportPirce = transportPirce;
+    }
+
+    public Date getBuyTime() {
+        return buyTime;
+    }
+
+    public void setBuyTime(Date buyTime) {
+        this.buyTime = buyTime;
+    }
+
+    public String getBuyAdmin() {
+        return buyAdmin;
+    }
+
+    public void setBuyAdmin(String buyAdmin) {
+        this.buyAdmin = buyAdmin;
+    }
+
+    public Integer getCollectRegion() {
+        return collectRegion;
+    }
+
+    public void setCollectRegion(Integer collectRegion) {
+        this.collectRegion = collectRegion;
+    }
+
+    public String getCollectAddress() {
+        return collectAddress;
+    }
+
+    public void setCollectAddress(String collectAddress) {
+        this.collectAddress = collectAddress;
+    }
+
+    public String getBuyRemark() {
+        return buyRemark;
+    }
+
+    public void setBuyRemark(String buyRemark) {
+        this.buyRemark = buyRemark;
+    }
+
+    public String getOrderRemark() {
+        return orderRemark;
+    }
+
+    public void setOrderRemark(String orderRemark) {
+        this.orderRemark = orderRemark;
+    }
+
+    public OrderState getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(OrderState orderState) {
+        this.orderState = orderState;
+    }
+}
---
+++ shopping-entity/src/main/java/edu/nf/shopping/order/entity/OrderState.java
@@ -0,0 +1,26 @@
+package edu.nf.shopping.order.entity;
+
+/**
+ * @author Achine
+ * @date 2020/3/4
+ */
+public class OrderState {
+    private Integer osId;
+    private String osName;
+
+    public Integer getOsId() {
+        return osId;
+    }
+
+    public void setOsId(Integer osId) {
+        this.osId = osId;
+    }
+
+    public String getOsName() {
+        return osName;
+    }
+
+    public void setOsName(String osName) {
+        this.osName = osName;
+    }
+}
